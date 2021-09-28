36030df635b9ceb886a04cc9a2bfbccc8e9d6bbe
crimeacs <crimeacs@yandex.ru>
2021-09-28 15:53:31 +0200

    fixed imports

--- seismic_augmentation/composition.py
+++ seismic_augmentation/composition.py
@@ -6,10 +6,7 @@ class BaseComposition(object):
         @param transforms: a list of transforms
         @param p: the probability of the transform being applied; default value is 1.0
         """
-        for transform in transforms:
-            assert isinstance(
-                transform, (BaseAugmentation, BaseComposition)
-            ), "Expected instances of type `BaseAugmentation` or `BaseComposition` for variable `transforms`"
+       
         assert 0 <= p <= 1.0, "p must be a value in the range [0, 1]"
 
         self.transforms = transforms
